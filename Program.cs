6f90d33df79a5155c05c77834b3e59b5215d6ee0
sske0 <akbaraz@code.edu.az>
2023-01-19 04:42:29 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace hw5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result;
+            Console.WriteLine("enter number1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("enter number2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+        initial: Console.WriteLine("enter opertaion (+,-,*,/): ");
+            string operation = Console.ReadLine();
+            Console.Clear();
+
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2; 
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "/":
+                    result = number1 / number2;
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("""bruh, no "mezelenmey" plz""");
+                    goto initial;
+            }
+            Console.WriteLine($"{number1} {operation} {number2} = {result}");
+
+        }
+    }
+}
